82a477cdbd1c5dc85a81664efb97fedd62890ffd
malmorarios <43325603+malmorarios@users.noreply.github.com>
2020-10-11 22:16:29 -0700

    Final clean-up
    deleted all extraneous, non-vital information/code; file is ready to be used as the starting point of the project!

--- main.py
+++ main.py
@@ -1,48 +1,27 @@
 from gensim.models import Word2Vec
 
-from time import time  # to time our operations
-import gzip, json # required to open dataset (gzip) and grab each data point (json) 
+from time import time 
+import gzip, json 
 
 import multiprocessing 
-cores = multiprocessing.cpu_count() # count the number of cores in a computer (used in creation of Word2Vec model)
-
-
-
-## RANDOM LINES OF CODE ##
-
-#import random
-#random.sample(all_poetryLines, 8)
-#w2v_model.init_sims(replace=True) # makes the model more memory efficient (pre-computes L2 norm)
-# run 'w2v_model.wv.vocab' to get a list of the words in the machine's vocabulary
+cores = multiprocessing.cpu_count()
 
 
 ### GLLOBAL VARIABLES ###
 
 create_w2vmodel = False
-
-""" w2v_model = Word2Vec(min_count=2, # ignores all words with total frequency lower than this
-                        window=2, # maximum distance between the current and predicted word within a sentence
-                        size=300, # dimensionality of word vectors
-                        sample=6e-5, # the threshold for configuring which higher-frequency words are randomly downsampled, useful range is (0, 1e-5)
-                        alpha=0.03, # initial learning rate
-                        min_alpha=0.0007, # learning rate will linearly drop to min_alpha as training progresses
-                        negative=20, # if >0, negative sampling will be used; specifies how many "noise words" should be drawn (usually between 5-20). If set to 0, no negative sampling is used.
-                        workers=cores-1)
- """
-# see full list of parameters here: https://radimrehurek.com/gensim/models/word2vec.html#gensim.models.word2vec.Word2VecVocab
+maxpoetrylines = 500
 
                     
-
 ### ----------------- ###
 
 
-def dataLoader():
+def dataLoader(limit):
     """ loads the Gutenberg Poetry dataset;
     output: a list of all poetry lines loaded """
 
-    print('\n==> Loading poetry line data...')
+    print('\n==> Loading poetry line data...\n')
 
-    limit = 500
     all_dataPoints = []
     for datapoint in gzip.open("gutenberg-poetry-v001.ndjson.gz"):
         all_dataPoints.append(json.loads(datapoint.strip()))
@@ -50,7 +29,6 @@ def dataLoader():
         if limit == 0:
             break 
     
-    # takes the string parts of the data (the poetry lines) and returns a list of the list of all words in each line of poetry
     all_poetryLines = [datapoint['s'].split() for datapoint in all_dataPoints]
 
     return all_poetryLines
@@ -59,7 +37,7 @@ def dataLoader():
 if __name__ == '__main__':
     
     ## LOADING THE DATA ##
-    all_poetryLines = dataLoader()
+    all_poetryLines = dataLoader(maxpoetrylines)
 
     if create_w2vmodel:
 
@@ -71,7 +49,7 @@ if __name__ == '__main__':
                         alpha=0.03,
                         min_alpha=0.0007,
                         negative=20,
-                        workers=cores-1)
+                        workers=cores-1) # https://radimrehurek.com/gensim/models/word2vec.html#gensim.models.word2vec.Word2VecVocab
         
         ## BUILDING VOCABULARY ##
         t = time()
